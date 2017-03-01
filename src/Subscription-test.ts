587242eb9cdc1727c772d58b64f77845b962157e
Kevin Zeng <zengfenfei@gmail.com>
2017-03-01 15:46:15 +0800

    Subscription refresh and keep alive

--- src/Subscription-test.ts
+++ src/Subscription-test.ts
@@ -9,13 +9,43 @@ async function test() {
     let restClient = new RestClient(config.app);
     await restClient.restoreToken(new FileTokenStore(config.tokenCacheFile));
     let sub = new Subscription(restClient);
+    sub.on('error', e => {
+        console.error('Subscription error', e);
+    });
     try {
         await sub.subscribe(['/restapi/v1.0/account/~/extension/~/presence']);
+        console.log('Subscribed');
         sub.on('notification', msg => {
-            console.log('>>>notification', msg);
+            console.log('>>>notification', msg.body.telephonyStatus, msg);
         });
     } catch (e) {
         console.error(e);
     }
-
 }
+
+/* 
+errorCode: 'TokenInvalid',
+message: 'Access token corrupted'
+
+async function testRefreshExpiredSubscription() {
+    let restClient = new RestClient(config.app);
+    await restClient.restoreToken(new FileTokenStore(config.tokenCacheFile));
+    let sub = new Subscription(restClient);
+    try {
+        await sub.subscribe(['/restapi/v1.0/account/~/extension/~/presence']);
+        sub.on('notification', msg => {
+            console.log('>>>notification', msg.body.telephonyStatus, msg);
+        });
+        setTimeout(() => {
+            console.log('The subscription should expire now.');
+            restClient.get('/subscription/' + sub.id).then(res => res.json()).then(subscription => {
+                console.log("get subscription", subscription);
+            });
+            sub.refresh().catch(e => {
+                console.error('Refresh error', e);
+            });
+        }, sub.expirationTime - Date.now());
+    } catch (e) {
+        console.error(e);
+    }
+}*/
--- src/Subscription.ts
+++ src/Subscription.ts
@@ -5,7 +5,11 @@ import RestClient from './RestClient';
 export default class Subscription extends EventEmitter {
 
     restClient: RestClient;
-    subscription: any;
+    id: string;
+    expirationTime: number; // Epoch time in ms.
+    eventFilters: string[];
+    refreshTimer: NodeJS.Timer;
+
     pubnub: any;
 
     constructor(restClient: RestClient) {
@@ -14,17 +18,44 @@ export default class Subscription extends EventEmitter {
     }
 
     /**
+     * TODO Retry after refresh error.
+     */
+    private setSubscription(subscription) {
+        this.id = subscription.id;
+        this.expirationTime = Date.parse(subscription.expirationTime);
+        this.eventFilters = subscription.eventFilters;
+        this.refreshTimer = setTimeout(() => {
+            this.refreshTimer = null;
+            this.refresh().catch(reason => {
+                this.clear();
+                let e = new Error('Subscription auto refresh error: ' + reason);
+                e['detail'] = reason;
+                this.emit('error', e);
+            });
+        }, this.expirationTime - Date.now() + refreshHandicap);
+    }
+
+    private clear() {
+        this.id = '';
+        this.expirationTime = 0;
+        this.eventFilters = null;
+        this.refreshTimer = null;
+    }
+
+    /**
      * expiresIn Optional. Subscription lifetime in seconds. Max value is 7 days (604800 sec)
      * The 'expiresIn' is not supported.
      */
     async subscribe(eventFilters: string[]) {
-        let res = await this.restClient.post('/subscription', { eventFilters, deliveryMode: { transportType: 'PubNub', encryption: true } });
-        this.subscription = await res.json();
-        let pubnub = new PubNub({ subscribeKey: this.subscription.deliveryMode.subscriberKey });
-        console.log('Subscription', this.subscription)
+        if (this.id) {
+            throw new Error('Subscription exists.');
+        }
+        let res = await this.restClient.post('/subscription', { eventFilters, deliveryMode });
+        let subscription = await res.json();
+        let pubnub = new PubNub({ subscribeKey: subscription.deliveryMode.subscriberKey });
         pubnub.addListener({
             message: msg => {
-                let decrypted = pubnub.decrypt(msg.message, this.subscription.deliveryMode.encryptionKey, {
+                let decrypted = pubnub.decrypt(msg.message, subscription.deliveryMode.encryptionKey, {
                     encryptKey: false,
                     keyEncoding: 'base64',
                     keyLength: 128,
@@ -33,17 +64,52 @@ export default class Subscription extends EventEmitter {
                 this.emit('notification', decrypted);
             },
             status: status => {
-                console.log('pubnub status event', status)
-            },
+                /*
+                Good response:
+                {   category: 'PNConnectedCategory',
+                    operation: 'PNSubscribeOperation',
+                    affectedChannels: [ '3455148789617805_dc8d7011' ],
+                    affectedChannelGroups: [] }
+
+                Wrong subscribeKey:
+                {   error: true,
+                    operation: 'PNSubscribeOperation',
+                    statusCode: 400,
+                    errorData: ...
+                    category: 'PNBadRequestCategory' }
+                */
+                if (status.error) {
+                    let e = new Error('PubNub status error, category: ' + status.category);
+                    e['detail'] = status;
+                    this.emit('error', e);
+                }
+            }/*,
             presence: presence => {
                 console.log('pubnub presence', presence);
-            }
+            }*/
         });
-        pubnub.subscribe({ channels: [this.subscription.deliveryMode.address] });
+        // Wrong address pubnub won't report error.
+        pubnub.subscribe({ channels: [subscription.deliveryMode.address] });
+        this.setSubscription(subscription);
         this.pubnub = pubnub;
+    }
+
+    private async refresh() {
+        if (!this.id) {
+            return;
+        }
+        if (Date.now() >= this.expirationTime) {
+            throw Error('Subscription expired, can not refresh.');
+        }
+        let res = await this.restClient.put('/subscription/' + this.id, { eventFilters: this.eventFilters, deliveryMode });
+        let subscription = await res.json();
+        this.setSubscription(subscription);
     }
 
 }
 
+const deliveryMode = { transportType: 'PubNub', encryption: true };
+// In ms
+const refreshHandicap = 30 * 1000;
 // In seconds
 //export const MAX_LIFETIME = 604800;
