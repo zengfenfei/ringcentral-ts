ddc94b1537331aaed0563dd8c55b86a8665e19ba
Kevin Zeng <zengfenfei@gmail.com>
2017-03-08 16:28:10 +0800

    Delete 'prepublish' script 'prepublish' script will execute after 'npm install', which will cause 'npm install' fail

---
+++ CONTRIBUTING.md
@@ -0,0 +1,5 @@
+# For Developers/Contributors
+
+## npm publish
+
+The content published to npm is in the `build/src` not the root directory. `npm publish` should be done in `build/src`. Generally, npm publish is done by travis, you should not do it manually.
--- script/update-package.json.js
+++ script/update-package.json.js
@@ -1,9 +1,8 @@
-let path= require('path');
+let path = require('path');
 let fs = require('fs');
 let pkg = require('../package.json');
 
 let output = './build/src/';
 pkg.main = path.relative(output, pkg.main);
 pkg.typings = path.relative(output, pkg.typings);
-delete pkg.scripts.prepublish;
-fs.writeFileSync(output+'package.json', JSON.stringify(pkg));
+fs.writeFileSync(output + 'package.json', JSON.stringify(pkg));
