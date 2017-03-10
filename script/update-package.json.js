b479231943204d042140db16c0a8b76358622e42
Kevin Zeng <zengfenfei@gmail.com>
2017-03-10 09:46:53 +0800

    Update package version to git tag before deploy

--- script/update-package.json.js
+++ script/update-package.json.js
@@ -2,9 +2,19 @@
 
 let path = require('path');
 let fs = require('fs');
+let child_process = require('child_process');
 let pkg = require('../package.json');
 
 let output = './build/src/';
 pkg.main = path.relative(output, pkg.main);
 pkg.typings = path.relative(output, pkg.typings);
+try {
+    updateVersion2gitTag(pkg);
+} catch (e) { }
 fs.writeFileSync(output + 'package.json', JSON.stringify(pkg));
+
+// Will throw if not on a tag
+function updateVersion2gitTag(pkg) {
+    let tag = child_process.execSync('git describe --tag').toString().trim();
+    pkg.version = tag;
+}
