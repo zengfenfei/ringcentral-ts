07dcbb9dd63af2dec1d0e5bcbf64ab764a70e62c
Kevin Zeng <zengfenfei@gmail.com>
2017-03-20 14:27:09 +0800

    Check refreshed subscription data by assert

--- src/Subscription.ts
+++ src/Subscription.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from 'events';
+import * as assert from 'assert';
 import * as PubNub from 'pubnub';
 import { OPERATIONS, CATEGORIES } from 'pubnub';
 import RestClient, { BASE_URL, API_VERSION } from './RestClient';
@@ -6,11 +7,15 @@ import RestClient, { BASE_URL, API_VERSION } from './RestClient';
 export default class Subscription extends EventEmitter {
 
 	rest: RestClient;
+
 	id: string;
 	expirationTime: number; // Epoch time in ms.
 	eventFilters: string[]; // Without REST base url
-	refreshTimer: NodeJS.Timer;
+	subscribeKey: string;
+	address: string;	// PubNub channels
+	encryptionKey: string;	// AES encryptionKey
 
+	refreshTimer: NodeJS.Timer;
 	pubnub: any;
 
 	constructor(restClient: RestClient) {
@@ -28,9 +33,20 @@ export default class Subscription extends EventEmitter {
 			// We should not check subscription id to tell if the subscription is canceled, because the subscription id does not exist before subscribe.
 			return;
 		}
-		this.id = subscription.id;
+		if (!this.id) {
+			this.id = subscription.id;
+			this.subscribeKey = subscription.subscribeKey;
+			this.address = subscription.address;
+			this.encryptionKey = subscription.encryptionKey;
+		} else {
+			assert(this.id === subscription.id, 'Subscription id should not change');
+			assert(this.subscribeKey === subscription.subscribeKey, 'SubscribeKey should not change');
+			assert(this.address === subscription.address, 'Subscription channel should not change');
+			assert(this.encryptionKey === subscription.encryptionKey, 'Subscription AES key should not change');
+		}
 		this.expirationTime = Date.parse(subscription.expirationTime);
 		this.eventFilters = unprefixFilters(subscription.eventFilters);
+
 		this.refreshTimer = setTimeout(() => {
 			this.refreshTimer = null;
 			this.refresh().catch(reason => {
