42ca9e1453e978f25efeef224a03bd27d5e6a077
Kevin Zeng <zengfenfei@gmail.com>
2017-04-26 16:23:58 +0800

    Subscription test: notify status error

--- test/subscription-reliability.ts
+++ test/subscription-reliability.ts
@@ -1,8 +1,6 @@
 import delay from 'delay.ts';
 import RingCentral from '../src/Client';
-import Subscription from '../src/Subscription';
 import FileTokenStore from '../src/FileTokenStore';
-import * as sinon from 'sinon';
 
 /*
 This file is to test the reliability and stability of the subscription.
@@ -28,6 +26,22 @@ async function test() {
 
 	checkStatus();
 	setInterval(checkStatus, 24 * 60 * 60 * 1000);
+
+	subscription.on('StatusError', err => {
+		console.log('@@@@Subscription status error', err);
+		notify('Subscription status error:' + JSON.stringify(err.detail, null, 4));
+		checkStatus();
+	});
+	subscription.on('status', status => {
+		console.log('@@@@Subscription pubnub status', status);
+		notify('Subscription status event:' + JSON.stringify(status, null, 4));
+	});
+	subscription.on('RefreshError', err => {
+		console.log('@@@@Subscription refresh error:' + err);
+	});
+	subscription.on('RefreshSuccess', () => {
+		console.log('@@@@Subscription refresh RefreshSuccess,' + new Date());
+	});
 
 	/**
 	 * Subscribe sms and presence event.
@@ -71,97 +85,3 @@ async function test() {
 		return ((Date.now() - startTime) / 1000 / 60 / 60 / 24).toFixed(2) + ' days';
 	}
 }
-
-return;
-
-let restClient: RingCentral;
-let subscription: Subscription;
-
-before(async () => {
-	subscription = new Subscription(restClient);
-	subscription.onMessage(msg => {
-		console.log('>>>Subscription message', msg);
-	});
-	subscription.on('StatusError', err => {
-		console.log('!!!Subscription status error', err);
-	});
-	subscription.on('status', status => {
-		console.log('Subscription pubnub status', status);
-	});
-	subscription.on('RefreshError', err => {
-		console.log('Subscription refresh error:' + err);
-	});
-	subscription.on('RefreshSuccess', () => {
-		console.log('Subscription refresh RefreshSuccess');
-	});
-});
-
-describe('Subscription', () => {
-
-	it.skip('should receive notifications forever', async () => {
-		let sub = subscription;
-		await sub.subscribe(['/account/~/extension/~/presence']);
-	});
-
-	it.skip('should not receive notification after subscription canceled', async () => {
-		let sub = subscription;
-		await sub.subscribe(['/account/~/extension/~/presence']);
-		await delay(5 * 1000);
-		await sub.cancel();
-	});
-
-	it.skip('should work when multiple instances created, and resubscribe after canceled', async () => {
-		let filters = ['/account/~/extension/~/presence'];
-		let sub = new Subscription(restClient);
-		sub.onMessage(msg => console.log('##message of first subscription', msg.body.telephonyStatus));
-
-		let sub2 = new Subscription(restClient);
-		sub2.onMessage(msg => console.log('@@message of second subscription', msg.body.telephonyStatus));
-		sub2.subscribe(filters);
-
-		let sub3 = new Subscription(restClient);
-		sub3.onMessage(msg => console.log('$$message of third subscription', msg.body.telephonyStatus));
-		sub3.subscribe(filters);
-
-		await sub.subscribe(filters);
-		await delay(800);
-		await sub.cancel();
-		await delay(3000);
-		await sub.subscribe(filters);
-		await sub.cancel();
-		await sub.subscribe(filters);
-
-		await delay(15 * 1000);
-		await sub.cancel();
-		await sub2.cancel();
-		await sub3.cancel();
-	});
-
-});
-
-/*
-errorCode: 'TokenInvalid',
-message: 'Access token corrupted'
-
-async function testRefreshExpiredSubscription() {
-    let restClient = new RestClient(config.app);
-    await restClient.restoreToken(new FileTokenStore(config.tokenCacheFile));
-    let sub = new Subscription(restClient);
-    try {
-        await sub.subscribe(['/account/~/extension/~/presence']);
-        sub.on('notification', msg => {
-            console.log('>>>notification', msg.body.telephonyStatus, msg);
-        });
-        setTimeout(() => {
-            console.log('The subscription should expire now.');
-            restClient.get('/subscription/' + sub.id).then(res => res.json()).then(subscription => {
-                console.log('get subscription', subscription);
-            });
-            sub.refresh().catch(e => {
-                console.error('Refresh error', e);
-            });
-        }, sub.expirationTime - Date.now());
-    } catch (e) {
-        console.error(e);
-    }
-}*/
